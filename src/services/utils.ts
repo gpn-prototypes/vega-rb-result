1f2635b2d95608db125bde2a0b03ac353de47fff
alekskond <adsnet@mail.ru>
2021-12-16 16:16:43 +0300

    Update utils.ts

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -3,13 +3,13 @@ import { ConceptionInput, RbProjectInput } from '@app/generated/graphql';
 export const getBaseApiUrl = (): string => {
   if (
     process.env.BASE_API_URL === undefined ||
-    process.env.BASE_API_URL === '/' ||
-    process.env.BASE_API_URL === ''
+    process.env.BASE_API_URL === "/" ||
+    process.env.BASE_API_URL === ""
   ) {
     return window.location.origin;
   }
-  
-  return process.env.BASE_API_URL.indexOf('://') > -1
+
+  return process.env.BASE_API_URL.indexOf("://") > -1
     ? process.env.BASE_API_URL
     : window.location.origin + process.env.BASE_API_URL;
 };
