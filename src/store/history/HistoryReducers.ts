1d3a59fc604b6bd3b8bdebf93c9bb0527e2978d1
Sergey Katugin <alex1dadigin@yandex.ru>
2022-03-16 12:23:24 +0000

    remove unused method

--- src/store/history/HistoryReducers.ts
+++ src/store/history/HistoryReducers.ts
@@ -3,8 +3,6 @@ import { reducerWithInitialState } from 'typescript-fsa-reducers';
 
 import { HistoryActions, HistoryStore } from './HistoryActions';
 
-export const DEFAULT_NUMBER_OF_ROWS = 50;
-
 export const historyInitialState: HistoryStore = {
   currentLocation: null,
 };
