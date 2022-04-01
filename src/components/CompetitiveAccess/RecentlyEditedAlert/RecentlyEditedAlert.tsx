5263620e0dfa0953dd44f5440c7f95b005f9efb4
Sergey Katugin <alex1dadigin@yandex.ru>
2022-04-01 09:45:40 +0000

    feat/VEGA-5124 fixes updated version of consta

--- src/components/CompetitiveAccess/RecentlyEditedAlert/RecentlyEditedAlert.tsx
+++ src/components/CompetitiveAccess/RecentlyEditedAlert/RecentlyEditedAlert.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Item } from '@consta/uikit/__internal__/src/components/SnackBar/helper';
 import { IconAlert } from '@consta/uikit/IconAlert';
-import { SnackBar } from '@consta/uikit/SnackBar';
+import { SnackBar, SnackBarItemDefault } from '@consta/uikit/SnackBar';
 
 import { cnAlert } from './cn-alert';
 
 import './RecentlyEditedAlert.css';
 
 export const RecentlyEditedAlert: React.FC = () => {
-  const items: Item[] = [
+  const items: SnackBarItemDefault[] = [
     {
       key: 1,
       message:
