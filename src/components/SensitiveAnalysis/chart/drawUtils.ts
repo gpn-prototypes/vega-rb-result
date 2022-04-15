7896e1cc8c28d1fbd3bfe0bdeebe000c07fbe3f4
Alexander <grachev>
2022-04-15 13:02:04 +0300

    feat/VEGA-5201 for demo

--- src/components/SensitiveAnalysis/chart/drawUtils.ts
+++ src/components/SensitiveAnalysis/chart/drawUtils.ts
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
+/* eslint-disable newline-per-chained-call */
 
-export namespace SensitiveAnalysisChart {
+export namespace DrawUtils {
   export interface Margin {
     top: number;
     right: number;
@@ -12,7 +13,7 @@ export namespace SensitiveAnalysisChart {
     top: 36,
     right: 0,
     bottom: 0,
-    left: 110,
+    left: 160,
   };
 
   export const Width = 651 - Margin.right - Margin.left;
@@ -23,318 +24,173 @@ export namespace SensitiveAnalysisChart {
     value: number;
     category: 0 | 1;
     percentile: number;
-    // color: 'orange' | 'blue';
-    // myValue: number;
-    // length: number;
-    // direction: 'left' | 'right';
   }
 
   export interface Options {
     format: string;
-    negative: string;
-    positive: string;
-    negatives: number[];
-    positives: number[];
     colors: Record<number, string>;
   }
 
   export const options: Options = {
     format: '',
-    negative: '',
-    positive: '',
-    negatives: [0],
-    positives: [1],
     colors: {
       0: '#F38B00',
       1: '#0AA5FF',
     },
   };
 
-  export type Series = d3.Series<{ [key: string]: number }, string>[];
-  export type Bias = [string, number];
+  // const groupByName = (data) => {
+  //   const result = data.reduce((acc, cur) => {
+  //     acc[cur.name] = acc[cur.name] || [];
+  //
+  //     acc[cur.name].push(cur);
+  //
+  //     return acc;
+  //   }, {});
+  //
+  //   return Object.values(result);
+  //
+  //   // [[value: 11, name: 'gfg'],[]]
+  // };
 
-  export function getAxisData(data: SensitiveAnalysisChart.Payload[]): {
-    series: Series;
-    bias: Bias[];
-    options: Options;
-  } {
-    // const options: Options = {
-    //   format: '',
-    //   negative: '',
-    //   positive: '',
-    //   negatives: [0],
-    //   positives: [1],
-    //   colors: {
-    //     0: '#F38B00',
-    //     1: '#0AA5FF',
-    //   },
-    // };
+  const formatValue = () => {
+    const format = d3.format(options.format || '');
 
-    const mappedNegatives = options.negatives.map((d: number) => [d, -1]);
-    const mappedPositives = options.positives.map((d: number) => [d, +1]);
+    return (innerX) => format(Math.abs(innerX));
+  };
 
-    /** TODO: Убрать все any, по быстрому не получилось исправить */
-    const signs = new Map<number[][], number[][]>([
-      ...(mappedNegatives as any),
-      ...(mappedPositives as any),
-    ]);
+  const getColor = (key) => {
+    return options.colors[key];
+  };
 
-    const series: Series = d3
-      .stack()
-      .keys([].concat([0] as any, [1] as any))
-      .value(([, value]: any, category: any) => {
-        console.log('value: ', value);
-        console.log('category: ', category);
+  export function tornadoChart(
+    currentData,
+    lol: number[][],
+    zp,
+    resultMinMax: number[][],
+    svg,
+    availableNames: string[],
+    currentHeight: number,
+  ) {
+    const sample: number[] = resultMinMax.flat(1);
 
-        return (signs.get(category) as any) * (value.get(category) || 0);
-      })
-      .offset(d3.stackOffsetDiverging)(
-      d3.rollups(
-        data,
-        (innerData) =>
-          d3.rollup(
-            innerData,
-            ([d]) => d.value,
-            (d) => d.category,
-          ),
-        (d) => d.name,
-      ) as any,
-    );
+    console.log(sample);
 
-    // TODO: посмотреть не ломаются ли биасы
-    const bias = d3.rollups(
-      data,
-      (v) =>
-        d3.sum(
-          v,
-          (d: any) => d.value * Math.min(0, signs.get(d.category.color) as any),
-        ),
-      (d) => d.name,
-    );
-
-    return { series, bias, options };
-  }
-
-  export function getAxisScale({
-    series,
-    resultMinMax,
-    bias,
-  }: {
-    series: SensitiveAnalysisChart.Series;
-    resultMinMax: number[][];
-    bias: SensitiveAnalysisChart.Bias[];
-  }): {
-    xScale: d3.ScaleLinear<number, number, never>;
-    x1Scale: d3.ScaleLinear<number, number, never>;
-    yScale: d3.ScaleBand<string>;
-  } {
-    const offset = 50;
-    const sample = resultMinMax.flat(1);
-
-    // выравнивания
-    const xScale = d3
-      .scaleLinear()
-      .domain(d3.extent(series.flat(2)) as any)
-      .rangeRound([
-        SensitiveAnalysisChart.Margin.left + offset,
-        SensitiveAnalysisChart.Width -
-          SensitiveAnalysisChart.Margin.right -
-          offset,
-      ]);
-    // выравнивания лево-право
-    const x1Scale = d3
-      .scaleLinear()
-      .domain([d3.min(sample) || 0, d3.max(sample) || 0])
-      .range([
-        SensitiveAnalysisChart.Margin.left,
-        SensitiveAnalysisChart.Width + SensitiveAnalysisChart.Margin.right,
-      ]);
-    // выравнивания, высота баров, отступы графика
     const heightMultiplier = 68;
-    const height =
-      bias.length * heightMultiplier +
-      SensitiveAnalysisChart.Margin.top +
-      SensitiveAnalysisChart.Margin.bottom;
 
-    const yScale = d3
+    const height =
+      availableNames.length * heightMultiplier +
+      DrawUtils.Margin.top +
+      DrawUtils.Margin.bottom;
+
+    // const x = d3
+    //   .scaleLinear()
+    //   .domain(currentData.map(({ value }) => value))
+    //   .range([DrawUtils.Margin.left, DrawUtils.Width]);
+
+    const x = d3
+      .scaleLinear()
+      .domain([0, d3.max(currentData, (d: any) => d.value) as any])
+      .range([DrawUtils.Margin.left, DrawUtils.Width]);
+
+    const x1 = d3
+      .scaleLinear()
+      .domain([0, zp.toFixed() * 2])
+      .range([DrawUtils.Margin.left, DrawUtils.Width]);
+
+    // const x2 = d3
+    //   .scaleLinear()
+    //   .domain([d3.min(sample) || 0, d3.max(sample) || 0])
+    //   .range([DrawUtils.Margin.left, DrawUtils.Width]);
+
+    const y = d3
       .scaleBand()
-      .domain(bias.map(([name]: any) => name))
-      .rangeRound([
-        SensitiveAnalysisChart.Margin.top,
-        height - SensitiveAnalysisChart.Margin.bottom,
-      ])
+      .domain(currentData.map(({ name }) => name))
+      .rangeRound([DrawUtils.Margin.top, height - DrawUtils.Margin.bottom])
       .padding(53 / heightMultiplier);
 
-    return { xScale, x1Scale, yScale };
-  }
-
-  export function getAxis({
-    xScale,
-    x1Scale,
-    yScale,
-    options,
-    currentPercentiles,
-    bias,
-    series,
-    data,
-  }: {
-    xScale: d3.ScaleLinear<number, number, never>;
-    x1Scale: d3.ScaleLinear<number, number, never>;
-    yScale: d3.ScaleBand<string>;
-    options: Options;
-    resultMinMax: number[][];
-    currentPercentiles: number[][];
-    bias: Bias[];
-    series: Series;
-    data: SensitiveAnalysisChart.Payload[];
-  }): {
-    xAxis;
-    yAxis;
-    y2Axis;
-    y3Axis;
-  } {
-    // const formatValue = () => {
-    //   const format = d3.format(options.format || '');
-    //
-    //   return (innerX) => format(Math.abs(innerX));
+    // const xAxisMiddleZeroPoint = (g) => {
+    //   return g
+    //     .attr('transform', `translate(0,${DrawUtils.Margin.top})`)
+    //     .attr('class', 'chart__xAxis')
+    //     .call(
+    //       d3
+    //         .axisTop(x1)
+    //         .scale(x1)
+    //         .ticks(1)
+    //         .tickFormat(formatValue())
+    //         .tickSizeOuter(0),
+    //     )
+    //     .call((innerG) => innerG.select('.domain').remove())
+    //     .call((innerG) =>
+    //       innerG
+    //         .selectAll('.tick')
+    //         .call((nestedG) => nestedG.selectAll('.tick line').remove())
+    //         .call((nestedG) => nestedG.selectAll('.tick text').remove())
+    //         /** цифры сверху */
+    //         .call((nestedG) =>
+    //           nestedG
+    //             .append('text')
+    //             .attr('style', 'font-weight: bold')
+    //             .attr(
+    //               'class',
+    //               'chart__text chart__text_middle chart__text_white',
+    //             )
+    //             .attr('text-anchor', 'middle')
+    //             .text((t: number) => t),
+    //         ),
+    //     );
     // };
 
-    const getPercentileByName = (name: string, isPositive = false): string => {
-      const currentElement = data.filter(
-        (currentData) => currentData.name === name,
-      );
-
-      return currentElement[isPositive ? 1 : 0].percentile
-        .toFixed(3)
-        .toString();
-    };
-
-    // смещение цифр на барах
-    const getPositiveTickByNegativeValue = (negativeValue: number): number => {
-      // const findPositiveIndexByNegative = () => {
-      //   // const negativeSeries = series[0];
-      //
-      //   const index = 0;
-      //
-      //   return index;
-      // };
-
-      // const rightBarSeries = series[1];
-
-      // const rightBarSeries = 0;
-
-      // const currentTick = rightBarSeries[findPositiveIndexByNegative()][1];
-
-      return 0;
-    };
-
-    /** Добавление оси X, а также добавление полосок */
-    const xAxis = (g) => null;
-    // g
-    //   .attr('transform', `translate(4,${SensitiveAnalysisChart.Margin.top})`)
-    //   .attr('class', 'chart__xAxis')
-    //   .call(
-    //     d3
-    //       .axisTop(x1Scale)
-    //       .ticks(3)
-    //       .tickFormat(formatValue())
-    //       .tickSizeOuter(0),
-    //   )
-    //   .call((innerG) => innerG.select('.domain').remove())
-    //   .call((innerG) =>
-    //     innerG
-    //       .selectAll('.tick')
-    //       .call((nestedG) => nestedG.selectAll('.tick line').remove())
-    //       .call((nestedG) => nestedG.selectAll('.tick text').remove())
-    //       // цифры сверху (2, 4, 6, 8)
-    //       // .call((nestedG) =>
-    //       //   nestedG
-    //       //     .append('text')
-    //       //     .attr('class', 'chart__text chart__text_middle')
-    //       //     .attr('text-anchor', 'middle')
-    //       //     .text((x: number) => x),
-    //       // )
-    //       // .data(resultMinMax.flat(1))
-    //       // пунктирные линии
-    //       // .append('line')
-    //       // .attr('transform', () => `translate(0, 20)`)
-    //       // .attr('y2', currentPercentiles.length * 66.6)
-    //       // .attr('stroke-dasharray', 3)
-    //       // .attr('stroke', 'rgba(246, 251, 253, 0.28)'),
-    //   );
-
-    /** Заглушка, показываем в левой части "1,0" + установка "zero point" */
-    const yAxis = (g) => null;
-    // g
-    //   /** Установка zero point */
-    //   .attr('transform', `translate(0,0)`)
-    //   .attr('class', 'chart__yAxisLeft')
-    //   .call(d3.axisLeft(yScale).tickSizeOuter(0))
-    //   .call((innerG) =>
-    //     innerG
-    //       .selectAll('.tick')
-    //       .data(bias)
-    //       .attr(
-    //         'transform',
-    //         ([name, min]) =>
-    //           `translate(${xScale(min) - 5},${
-    //             (yScale(name) || 0) + yScale.bandwidth() + 13
-    //           })`,
-    //       )
-    //       .text('')
-    //       .append('text')
-    //       .attr('class', 'chart__text chart__text_white')
-    //       .text(([name]) => getPercentileByName(name)),
-    //   )
-    //   /** Установка позиции zero point */
-    //   .call((innerG) =>
-    //     innerG
-    //       .select('.domain')
-    //       .attr('display', 'none')
-    //       .attr('transform', `translate(${xScale(0)},0)`),
-    //   );
-
-    /** Заглушка, показываем в правой части "1,0" */
-    const y2Axis = (g) =>
-      g
-        .attr('transform', `translate(0,0)`)
-        .attr('class', 'chart__yAxisRight')
-        .call((innerG) =>
-          innerG
-            .call(d3.axisLeft(yScale).tickSizeOuter(0))
-            .selectAll('.tick')
-            .data(bias)
-            .attr('transform', ([name, min]) => {
-              return `translate(${
-                xScale(getPositiveTickByNegativeValue(min)) + 5
-              },${(yScale(name) || 0) + yScale.bandwidth() + 13})`;
-            })
-            .text('')
-            .append('text')
-            .attr('class', 'chart__text chart__text_start chart__text_white')
-            .text(([name]) => {
-              return getPercentileByName(name, true);
-            }),
+    const xAxis = (g) => {
+      return g
+        .attr('transform', `translate(0,${DrawUtils.Margin.top})`)
+        .attr('class', 'chart__xAxis')
+        .call(
+          d3
+            .axisTop(x1)
+            .scale(x1)
+            .ticks(3)
+            .tickFormat(formatValue())
+            .tickSizeOuter(0),
         )
-        .call((innerG) => innerG.select('.domain').attr('display', 'none'));
-
-    /** Обозначение слева(названия F, Kн) */
-    const y3Axis = (g) =>
-      g
-        .attr('transform', `translate(0,0)`)
-        .attr('class', 'chart__yAxisLeftName')
+        .call((innerG) => innerG.select('.domain').remove())
         .call((innerG) =>
           innerG
-            .call(d3.axisLeft(yScale))
+            .selectAll('.tick')
+            .call((nestedG) => nestedG.selectAll('.tick line').remove())
+            .call((nestedG) => nestedG.selectAll('.tick text').remove())
+            /** цифры сверху */
+            .call((nestedG) =>
+              nestedG
+                .append('text')
+                .attr('class', 'chart__text chart__text_middle')
+                .attr('text-anchor', 'middle')
+                .text((t: number) => t),
+            )
+            /** пунктирные линии */
+            .append('line')
+            .attr('transform', () => `translate(0, 20)`)
+            .attr('y2', availableNames.length * 66.6)
+            .attr('stroke-dasharray', 3)
+            .attr('stroke', 'rgba(246, 251, 253, 0.28)'),
+        );
+    };
+
+    /** Названия слева */
+    const yAxis = (g) => {
+      return g
+        .attr('transform', `translate(0, -14)`)
+        .call((innerG) =>
+          innerG
+            .attr('class', 'chart__text chart__text_start chart__yAxisLeftName')
+            .attr('data-testid', 'chart-names-container')
+            .call(d3.axisLeft(y))
             .call((nestedG) => nestedG.select('.domain').remove())
             .selectAll('.tick')
-            .data(bias)
-            .attr('class', 'chart__text chart__text_start')
-            .attr(
-              'transform',
-              ([name]) =>
-                `translate(0, ${(yScale(name) || 0) + yScale.bandwidth() / 2})`,
-            ),
+            .selectAll('line')
+            .remove()
+            .data(currentData[0]),
         )
         .call((nestedG) => nestedG.selectAll('.chart__text line').remove())
         .call((nestedG) =>
@@ -343,12 +199,164 @@ export namespace SensitiveAnalysisChart {
             .attr('x', '0')
             .attr('text-anchor', 'end'),
         );
-
-    return {
-      xAxis,
-      yAxis,
-      y2Axis,
-      y3Axis,
     };
+
+    svg
+      .attr(
+        'width',
+        DrawUtils.Width + DrawUtils.Margin.left + DrawUtils.Margin.right,
+      )
+      .attr(
+        'height',
+        currentHeight + DrawUtils.Margin.top + DrawUtils.Margin.bottom,
+      );
+
+    svg.append('g').call(yAxis);
+    svg.append('g').call(xAxis);
+    // svg.append('g').call(xAxisMiddleZeroPoint);
+
+    let maxvalue;
+
+    function chart(selection) {
+      selection.each((data) => {
+        maxvalue =
+          Math.abs(
+            d3.extent(data, (d: any) => {
+              return d.value;
+            })[0] as any,
+          ) >
+          Math.abs(
+            d3.extent(data, (d: any) => {
+              return d.value;
+            })[1] as any,
+          )
+            ? Math.abs(
+                d3.extent(data, (d: any) => {
+                  return d.value;
+                })[0] as any,
+              )
+            : Math.abs(
+                d3.extent(data, (d: any) => {
+                  return d.value;
+                })[1] as any,
+              );
+        x.domain([maxvalue * -1, maxvalue]);
+        y.domain(
+          data.map((d) => {
+            return d.name;
+          }),
+        );
+
+        const bar = svg
+          .append('g')
+          .attr('data-testid', 'chart-bars-container')
+          .selectAll('.bar')
+          .data(data);
+
+        bar
+          .enter()
+          .append('rect')
+          .attr('class', (d) => {
+            return `bar bar--${d.value < 0 ? 'negative' : 'positive'}`;
+          })
+          .attr('x', (d) => {
+            return x(Math.min(0, d.value));
+          })
+          .attr('y', (d) => {
+            return y(d.name);
+          })
+          //   .attr('y', ({ data: [name] }: any) => yScale(name) || 0)
+          .attr('rx', () => 2)
+          .attr('width', (d) => {
+            return Math.abs(x(d.value) - x(0));
+          })
+          .attr('data-testid', (d) => {
+            return d.name;
+          })
+          .attr('style', (d) => {
+            return `fill: ${getColor(d.category)}`;
+          })
+          .attr('height', y.bandwidth());
+        bar
+          .enter()
+          .append('text')
+          .attr(
+            'class',
+            (d) =>
+              `chart__text chart__text_white ${
+                d.value > 0 && 'chart__text_start'
+              }`,
+          )
+          .attr('text-anchor', 'end')
+          /** Цифры на барах */
+          .attr('x', (d) => {
+            // const sortedD = groupByName(d);
+            // const arr = [
+            //   [-12, 33],
+            //   [-40, 20],
+            // ];
+
+            // const cloneD = { ...d };
+
+            // console.log(index);
+            // arr.push;
+
+            let titlePlacementX = 0;
+
+            if (d.value > 0) {
+              titlePlacementX =
+                Math.abs(x(d.value) - x(0)) + x(Math.min(0, d.value)) + 5;
+            }
+
+            // console.log(d);
+            // if (d.value < 0) {
+            //   titlePlacement = x(Math.min(0, d.value)) - 5;
+            // }
+
+            // if ((d[index][0] && lol[0][index][1]) > 0) {
+            //   console.log(index);
+            // }
+            //
+            // if (Math.abs(x(d.value) - x(0)) < 30 && d.value > 0)
+            //   titlePlacement = x(d.value) - 30;
+            else if (d.value < 0) {
+              titlePlacementX = x(Math.min(0, d.value)) - 5;
+            }
+
+            return titlePlacementX;
+          })
+          .attr('data-testid', (d) => {
+            return d.name;
+          })
+          .attr('y', (d) => {
+            return (y(d.name) as any) + y.bandwidth() / 2;
+          })
+          .attr('dy', (d) => {
+            let baseTitlePlacementY = '.35em';
+
+            if (d.value > 0) {
+              if (d.category === 0) {
+                baseTitlePlacementY = '1.350em';
+              } else {
+                baseTitlePlacementY = '-1.350em';
+              }
+            } else if (d.value < 0) {
+              if (d.category === 1) {
+                baseTitlePlacementY = '-1.350em';
+              } else {
+                baseTitlePlacementY = '1.350em';
+              }
+            }
+
+            return baseTitlePlacementY;
+            // return '.35em';
+          })
+          .text((d) => {
+            return d.percentile.toFixed(3);
+          });
+      });
+    }
+
+    return chart;
   }
 }
