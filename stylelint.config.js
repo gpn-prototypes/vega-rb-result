a28e9f90cf03a1bd75ad532e6b73d17856021fc8
Sergey Katugin <sergey.katugin@pwc.ru>
2022-04-27 12:39:17 +0300

    update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   // eslint-disable-next-line global-require
   ...require('@gpn-prototypes/frontend-configs/stylelintrc'),
-  ...{
-    rules: {
-      'selector-class-pattern': null,
-    },
-  },
 };
