f4be0c982dad833d84a41749cef4c3a7f2daec1e
Sergey Katugin <sergey.katugin@pwc.ru>
2022-06-03 14:01:27 +0500

    hotfix fix names with commas

--- src/utils/__tests__/UnpackTableData.test.tsx
+++ src/utils/__tests__/UnpackTableData.test.tsx
@@ -100,7 +100,7 @@ describe('UnpackTableData', () => {
 
     expect(nameWithParents).toEqual('концепция 1');
     expect(nameWithParentsSecondRow).toEqual('Архангеловский');
-    expect(nameWithParentsNotRoot).toEqual('концепция 1,Архангеловский');
+    expect(nameWithParentsNotRoot).toEqual('концепция 1_&888&_Архангеловский');
     expect(nameWithParentsNotRoot2).toEqual(
       'концепция 1,Архангеловский,2_Месторождение',
     );
